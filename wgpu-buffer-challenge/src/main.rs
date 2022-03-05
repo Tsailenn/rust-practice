bb6342ceb7654bc8599141609c489e627ff9a712
Tsailenn <70084579+Tsailenn@users.noreply.github.com>
2022-03-05 08:22:37 -0800

    finish wgpu buffer challenge

--- wgpu-buffer-challenge/src/main.rs
+++ wgpu-buffer-challenge/src/main.rs
@@ -1,3 +1,5 @@
+use std::{array::IntoIter, fmt::Debug};
+
 use wgpu::util::DeviceExt;
 use winit::{
     event::*,
@@ -10,6 +12,10 @@ use winit::{
 struct Vertex {
     pos: [f32; 3],
     color: [f32; 3],
+}
+
+struct Triangle {
+    indices: [u16; 3],
 }
 
 impl Vertex {
@@ -25,6 +31,20 @@ impl Vertex {
     }
 }
 
+impl Triangle {
+    fn as_flat(arr: &Vec<Triangle>) -> Vec<u16> {
+        let mut res: Vec<u16> = Vec::new();
+
+        arr.iter().for_each(|t| {
+            t.indices.iter().for_each(|el| {
+                (&mut res).push(el.clone());
+            });
+        });
+
+        res
+    }
+}
+
 struct GameState {
     instance: wgpu::Instance,
     adapter: wgpu::Adapter,
@@ -34,11 +54,13 @@ struct GameState {
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
     render_pipelines: Vec<wgpu::RenderPipeline>,
-    buttons_pressed: Vec<VirtualKeyCode>,
+    vertices: Vec<Vertex>,
+    triangles: Vec<Triangle>,
+}
+
+struct Buffers {
     vertex_buffer: wgpu::Buffer,
     index_buffer: wgpu::Buffer,
-    vertices: Vec<Vertex>,
-    indices: Vec<u16>,
 }
 
 fn main() {
@@ -47,6 +69,56 @@ fn main() {
     env_logger::init();
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let mut state = pollster::block_on(GameState::new(&window));
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => {
+            if !state.input(event) {
+                match event {
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            },
+                        ..
+                    } => *control_flow = ControlFlow::Exit,
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
+                    }
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        // new_inner_size is &&mut so we have to dereference it twice
+                        state.resize(**new_inner_size);
+                    }
+                    _ => {}
+                }
+            }
+        }
+        Event::RedrawRequested(window_id) if window_id == window.id() => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                // Reconfigure the surface if lost
+                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                // The system is out of memory, we should probably quit
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                // All other errors (Outdated, Timeout) should be resolved by the next frame
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+        Event::MainEventsCleared => {
+            // RedrawRequested will only trigger once, unless we manually
+            // request it.
+            window.request_redraw();
+        }
+        _ => {}
+    });
 }
 
 impl GameState {
@@ -139,22 +211,85 @@ impl GameState {
             multiview: None, // 5.
         });
 
-        todo!();
+        GameState {
+            instance,
+            adapter,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            render_pipelines: vec![render_pipeline],
+            vertices: vec![],
+            triangles: vec![],
+        }
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
-        let res = false;
+        let mut res = false;
 
-        for b in self.buttons_pressed.iter() {
-            match b {
-                VirtualKeyCode::Space => {
-                    println!("space pressed");
-                    todo!();
+        match event {
+            WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state,
+                        virtual_keycode: Some(VirtualKeyCode::Space),
+                        ..
+                    },
+                ..
+            } => {
+                if state == &ElementState::Pressed {
+                    //pressed
+                    self.vertices = vec![
+                        Vertex {
+                            pos: [-0.0868241, 0.49240386, 0.0],
+                            color: [1.0, 0.0, 0.5],
+                        }, // A
+                        Vertex {
+                            pos: [-0.49513406, 0.06958647, 0.0],
+                            color: [0.5, 1.0, 0.5],
+                        }, // B
+                        Vertex {
+                            pos: [-0.21918549, -0.44939706, 0.0],
+                            color: [0.5, 0.0, 1.0],
+                        }, // C
+                        Vertex {
+                            pos: [0.35966998, -0.3473291, 0.0],
+                            color: [0.5, 0.0, 0.5],
+                        }, // D
+                        Vertex {
+                            pos: [0.44147372, 0.2347359, 0.0],
+                            color: [0.5, 0.0, 0.5],
+                        }, // E
+                    ];
+
+                    self.triangles = vec![
+                        Triangle { indices: [0, 1, 4] },
+                        Triangle { indices: [1, 2, 4] },
+                        Triangle { indices: [2, 3, 4] },
+                    ]
+                } else {
+                    //not pressed
+                    self.vertices = vec![
+                        Vertex {
+                            pos: [0.5, -0.5, 0.0],
+                            color: [1.0, 0.0, 0.0],
+                        },
+                        Vertex {
+                            pos: [0.0, 0.5, 0.0],
+                            color: [0.0, 1.0, 0.0],
+                        },
+                        Vertex {
+                            pos: [-0.5, -0.5, 0.0],
+                            color: [0.0, 0.0, 1.0],
+                        },
+                    ];
+
+                    self.triangles = vec![Triangle { indices: [0, 1, 2] }]
                 }
-                _ => {
-                    println!("nothing");
-                }
+                res = true;
             }
+            _ => {}
         }
 
         res
@@ -172,15 +307,68 @@ impl GameState {
     }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        let vertex_buffer = self
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
             .device
-            .create_buffer_init(&wgpu::util::BufferInitDescriptor {
-                label: Some("Vertex Buffer"),
-                contents: bytemuck::cast_slice(self.vertices.as_slice()),
-
-                usage: wgpu::BufferUsages::VERTEX,
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
             });
 
-        todo!();
+        let index_array = Triangle::as_flat(&self.triangles);
+        let buffers = Buffers {
+            vertex_buffer: self
+                .device
+                .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                    label: Some("Vertex Buffer"),
+                    contents: bytemuck::cast_slice(self.vertices.as_slice()),
+                    usage: wgpu::BufferUsages::VERTEX,
+                }),
+            index_buffer: self
+                .device
+                .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                    label: Some("Index Buffer"),
+                    contents: bytemuck::cast_slice(index_array.as_slice()),
+                    usage: wgpu::BufferUsages::INDEX,
+                }),
+        };
+
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[
+                    // This is what [[location(0)]] in the fragment shader targets
+                    wgpu::RenderPassColorAttachment {
+                        view: &view,
+                        resolve_target: None,
+                        ops: wgpu::Operations {
+                            load: wgpu::LoadOp::Clear(wgpu::Color {
+                                r: 0.1,
+                                g: 0.2,
+                                b: 0.3,
+                                a: 1.0,
+                            }),
+                            store: true,
+                        },
+                    },
+                ],
+                depth_stencil_attachment: None,
+            });
+
+            //self.temp_vertex_buffer = Some(vertex_buffer);
+            //self.temp_index_buffer = Some(index_buffer);
+
+            render_pass.set_pipeline(&self.render_pipelines[0]);
+            render_pass.set_vertex_buffer(0, buffers.vertex_buffer.slice(..));
+            render_pass.set_index_buffer(buffers.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..index_array.len() as u32, 0, 0..1);
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
     }
 }
